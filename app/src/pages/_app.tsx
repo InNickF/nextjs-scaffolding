5d6f5ec647566fefc795bd6aae50ef330f4a8f1d
Nick Fuenmayor <in.nickf@gmail.com>
2022-01-08 19:33:07 -0500

    First commit (general approach added)

---
+++ app/src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../commons/styles/globals.css'
+import { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
